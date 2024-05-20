4f295ad7d985af90477dd3cfb045d48cba648d06
umaradil02 <158709535+umaradil02@users.noreply.github.com>
2024-05-21 02:00:44 +0500

    navbar setting

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
-function classToggle() {
-    const navs = document.querySelectorAll('.navbar')
 
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let year = document.querySelector(".date");
+year.innerHTML = new Date().getFullYear();
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');  
+    menuIcon.classList.toggle('fa-bars');  
+    navbar.classList.toggle("nav_active");
 }
 
-document.querySelector('#menu-icon')
-    .addEventListener('click', classToggle);
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav ul li a");
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+        console.log(id)
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove("nav_active");
+            });
+            document.querySelector(`header nav ul li a[href*="${id}"]`).classList.add("nav_active");
+        
+        }
+    });
+
     
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
+
+    menuIcon.classList.remove("fa-xmark");  
+    menuIcon.classList.add("fa-bars");  
+    navbar.classList.remove("nav_active");
+};
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+ScrollReveal().reveal('.home-content, heading', { origin: "top" });
+ScrollReveal().reveal('.home-img, .services-container, .fortfolio-box, .contact form', { origin: "bottom" });
+ScrollReveal().reveal('.home-content h1, .about-img ', { origin: "left" });
+ScrollReveal().reveal('.home-content p, .abou-content ', { origin: "right" });
+
+const typed = new Typed('.multiple-text',{
+    strings:["Frontent Designer","Web Designer","UI / UX Designer","Graphic Designer","Digital Marketer"],
+    typeSpeed: 70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+});
